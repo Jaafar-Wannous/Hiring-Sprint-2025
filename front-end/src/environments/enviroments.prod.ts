fb4b22b3e1eb6c9444aba3fe9ff984e91b3d73da
Jaafar-Wannous <jaafarwannous1@gmail.com>
2025-11-15 15:54:05 +0200

    fix enviroment.ts

--- front-end/src/environments/enviroments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://hiring-sprint-2025-1-saex.onrender.com/api',
-};
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  production: true,
+  apiUrl: 'https://hiring-sprint-2025-1-saex.onrender.com/api',
 };
